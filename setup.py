1f24821f8bb6531f2c1d78c2854b974127e4574e
Abhinav Khajurgi <abhinav200501@gmail.com>
2026-02-08 17:29:44 +0530

    Added EDA, feature engineering, and clustering notebooks

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+HYPEN_E_DOT = '-e .'
 
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        # Clean up newline characters
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        # Remove '-e .' if present to prevent recursive loops
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
+    
     return requirements
 
-
-
-
-
-
 setup(
-name='CUSTOMER-SEGMENTATION',
-version='0.0.1',
-author='AbhinavKhajurgi',
-author_email='abhinav.khajurgi23@spit.ac.in',
-packages=find_packages(),
-install_requires=get_requirements('reqirements.txt')
+    name='CUSTOMER-SEGMENTATION',
+    version='0.0.1',
+    author='AbhinavKhajurgi',
+    author_email='abhinav.khajurgi23@spit.ac.in',
+    packages=find_packages(),
+    # FIXED THE TYPO BELOW
+    install_requires=get_requirements('requirements.txt')
 )
